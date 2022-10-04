a28e735fffe9f70f913513ef8effd1a1cee9126f
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-10-04 18:02:05 +0200

    add some fix

--- Models/Pizza.cs
+++ Models/Pizza.cs
@@ -4,8 +4,8 @@ public class Pizza
 {
     
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public string Photo { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public string? Photo { get; set; }
     public decimal Price { get; set; }
 }
